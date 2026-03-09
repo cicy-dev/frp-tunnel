8f46ac4c895f43c56697eb576b67e2c8c319482c
w3c_offical <w3c.offical@gmail.com>
2026-03-09 17:27:34 +0800

    auto commit Mon Mar  9 17:27:34 CST 2026

--- frp_tunnel/cli.py
+++ frp_tunnel/cli.py
@@ -270,6 +270,7 @@ def server_status():
         console.print(f"   [yellow]pip install frp-tunnel[/yellow]")
         console.print(f"   [yellow]pip install frp-tunnel -i https://pypi.tuna.tsinghua.edu.cn/simple[/yellow]  # 国内镜像")
         console.print(f"   [yellow]pip install git+https://github.com/cicy-dev/frp-tunnel.git[/yellow]  # GitHub 最新版")
+        console.print(f"   [yellow]pip install https://gh-proxy.com/https://github.com/cicy-dev/frp-tunnel/archive/refs/heads/main.zip[/yellow]  # 国内加速")
     console.print()
 
 @server.command('install')
